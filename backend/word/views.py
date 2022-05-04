7608c01b04daa2ac7f0f0d7b678370ee3f52d8a5
urivat <rashad.porter18@gmail.com>
2022-05-04 15:12:44 -0400

    I added the model and searilizer for the word model + made migrations to the database

---
+++ backend/word/views.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Word
+from .serializers import 
+
+# Create your views here.
